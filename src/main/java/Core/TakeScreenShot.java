9df9077dd23efa037518c6698d25a0882f27c259
wisamokkeh <wisamokkeh2@gmail.com>
2022-11-07 22:00:52 +0200

    add pom file and core package

---
+++ src/main/java/Core/TakeScreenShot.java
@@ -0,0 +1,29 @@
+package Core;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class TakeScreenShot {
+	WebDriver driver;
+	public TakeScreenShot(WebDriver driver) {
+		this.driver = driver;
+	}
+	public void takeScreenShot(String filePath) throws IOException {
+		TakesScreenshot scrShot =((TakesScreenshot)this.driver);
+		File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+		
+		//Move image file to new destination
+
+		File DestFile=new File(filePath);
+
+		//Copy file at destination
+
+		FileUtils.copyFile(SrcFile, DestFile);
+
+	}
+}
